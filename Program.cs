682f9ce49f67c0b37ed4e3ca171424b8e9ae7e97
Muhammad Abrar Khan <makabrarkhan110@gmail.com>
2023-10-29 17:54:48 +0500

    first Commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyLexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+Console.WriteLine("Enter a string:");
+string input = Console.ReadLine() ?? string.Empty;
+
+            
+            List<string> tokens = Tokenize(input);
+            
+            Console.WriteLine("Tokens:");
+            foreach (var token in tokens)
+            {
+                Console.WriteLine(token);
+            }
+        }
+static List<string> Tokenize(string? input)
+{
+    if (string.IsNullOrEmpty(input))
+    {
+        return new List<string>();
+    }
+    // Splitting by space for simplicity. You can enhance this.
+    return new List<string>(input.Split(' '));
+}
+
+    }
+}
